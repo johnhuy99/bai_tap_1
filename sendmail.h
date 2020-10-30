82d5c27bb657fe1af39f000d9f508f41f1c829db
Xuan_Phung <73245699+johnhuy99@users.noreply.github.com>
2020-10-30 23:27:40 +0700

    Add files via upload

---
+++ sendmail.h
@@ -0,0 +1,23 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+#endif
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+int sendEmail(char* contentEmail, char* mail_receive)
+{
+	char cmd[100];  // to hold the command.
+    char tempFile[100];     // name of tempfile.
+
+    strcpy(tempFile,tempnam("/tmp","sendmail")); // generate temp file name.
+
+    FILE *fp = fopen(tempFile,"w"); // open it for writing.
+    fprintf(fp,"%s\n",contentEmail);        // write body to it.
+    fclose(fp);             // close it.
+
+    sprintf(cmd,"sendmail %s < %s",mail_receive,tempFile); // prepare command.
+    system(cmd);     // execute it.
+
+    return 0;
+}
